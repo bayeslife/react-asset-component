e153083ca79c58026215894fac9c0f4a46983bcf
Phil Tomlinson <ptomlinson@quartileone.com>
2019-05-10 12:36:32 +1000

    Now use archetype

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import Lib  from '../../src/index.js'
+
+const AssetStatefulComponent = Lib.AssetStatefulComponent
 
 class Demo extends Component {
   render() {
     return <div>
       <h1>short-interval-constrol-react-assets-component Demo</h1>
-      <Example url="http://localhost:3001/assets"/>
+      <AssetStatefulComponent url="http://localhost:3001/assets"/>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
-import React, { useReducer, useEffect } from 'react';
-
-import axios from 'axios'
 import AssetComponent from './component-assets';
-import { reducer as reducerSelectAsset, click } from './reducer-selectasset';
-import { reducer as reducerAsset, replace as replaceAssets } from './reducer-assetset';
+import AssetStatefulComponent from './component-assets-stateful';
 
-var theassets = [{key: 'testasset',description: 'test asset'}]
-
-const ContainerComponent = ({url}) => {
-  const [selected, dispatch] = useReducer(reducerSelectAsset, null)
-  const [assets,setAssets] = useReducer(reducerAsset, theassets)
-  
-  const fetch = async () =>  {
-    const result = await axios(url)
-    setAssets(replaceAssets(result.data))
-  }
-
-  useEffect(() => {  
-    fetch()
-  }, []);
-
-
-  return <AssetComponent
-    assets={ assets }
-    selected={ selected }
-    onClick={(selected) => dispatch(click(selected.target.innerText))}
-  />
-}
-
-export default ContainerComponent
+export default {
+  AssetComponent,
+  AssetStatefulComponent
+}  
